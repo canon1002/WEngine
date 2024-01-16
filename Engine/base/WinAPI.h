0c334b8205bb8009286389329b587f525f6dd1e2
canon1002 <k022g0038@g.neec.ac.jp>
2024-01-16 14:31:05 +0900

    外部ファイルの位置を変更

--- Engine/base/WinAPI.h
+++ Engine/base/WinAPI.h
@@ -19,9 +19,9 @@
 #pragma comment(lib,"winmm.lib")
 
 // ImGui
-#include "../../../externals/imgui/imgui.h"
-#include "../../../externals/imgui/imgui_impl_dx12.h"
-#include "../../../externals/imgui/imgui_impl_win32.h"
+#include "../../Externals/imgui/imgui.h"
+#include "../../Externals/imgui/imgui_impl_dx12.h"
+#include "../../Externals/imgui/imgui_impl_win32.h"
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 // Comポインタ
