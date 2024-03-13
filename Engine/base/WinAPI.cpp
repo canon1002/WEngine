71c5ce37d41f9453a3b4cf9292e4c554fc829653
canon1002 <k022g0038@g.neec.ac.jp>
2024-03-13 17:24:33 +0900

    シェーダーの不具合修正完了!

--- Engine/base/WinAPI.cpp
+++ Engine/base/WinAPI.cpp
@@ -134,7 +134,7 @@ Microsoft::WRL::ComPtr <IDxcBlob> WinAPI::CompileShader(
 		filePath.c_str(),		//コンパイル対象のhlslファイル名
 		L"-E",L"main",			// エントリーポイントの指定。基本的にmain以外にはしない。
 		L"-T",profile,			// ShaderProfileの設定
-		L"-Zi",L"Qembed_debug",	// デバッグ用の情報を埋め込む
+		L"-Zi",L"-Qembed_debug",	// デバッグ用の情報を埋め込む
 		L"-Od",					// 最適化を外しておく
 		L"-Zpr",				// コンパイル結果
 	};
