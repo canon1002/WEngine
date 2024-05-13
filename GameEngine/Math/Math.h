c8d7eca507e350fa482ce1e703875d17d85a83ee
canon1002 <k022g0038@g.neec.ac.jp>
2024-05-13 10:44:08 +0900

    Trasform追加

--- GameEngine/Math/Math.h
+++ GameEngine/Math/Math.h
@@ -4,3 +4,21 @@
 #include"Vec4.h"
 #include"Matrix4x4.h"
 #include"Quaternion.h"
+
+/// <summary>
+/// 回転がVector3で表されるTransform構造体
+/// </summary>
+struct EulerTransform {
+	Vector3 scale_;			// 拡大率
+	Vector3 rotation_;		// 回転
+	Vector3 translation_;	// 平行移動
+};
+
+/// <summary>
+/// 回転がQuaternionで表されるTransform構造体
+/// </summary>
+struct QuaternionTransform {
+	Vector3 scale_;			// 拡大率
+	Quaternion rotation_;	// 回転
+	Vector3 translation_;	// 平行移動
+};
